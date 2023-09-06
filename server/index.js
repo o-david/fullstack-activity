f58b7e750842034deb7325ffabcbe8de6afed559
David <okekedavid1333@gmail.com>
2023-09-06 16:47:09 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+//const express = require("express");
+import sql from "./db.js";
+import cors from "cors";
+
+const app = express();
+
+const data = [
+  { id: "1", task: "Take a bath", is_completed: true },
+  { id: "2", task: "Eatttttt", is_completed: false },
+  { id: "3", task: "Eas some more", is_completed: false },
+  { id: "4", task: "Rest", is_completed: true },
+];
+
+app.get("/", (req, res) => {
+  res.send("It works");
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+
+app.get("/api/todos", (req, res) => {
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
